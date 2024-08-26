8aec5d312cd8d5f3e4b6f39f40bf7e62fc95a097
Kartikscript <kartik.13102003@gmail.com>
2024-08-26 13:08:14 +0530

    fixing..

--- middleware.ts
+++ middleware.ts
@@ -15,12 +15,28 @@
 // }
 
 // import { authMiddleware } from "@clerk/nextjs";
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
  
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-});
+// export default authMiddleware({
+//   publicRoutes: ['/', '/api/webhooks/clerk', '/sign-up','/sign-in'],
+  
+// });
  
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)",'/sign-up','/sign-in'],
+// };
+
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware((auth) => {
+  auth().protect()
+})
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
