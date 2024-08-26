88382e860fd7ef0e07a9fb03f74ce5c3cf4753d3
Kartikscript <kartik.13102003@gmail.com>
2024-08-26 13:13:02 +0530

    fixing..

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -4,5 +4,5 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+const sql = neon(process.env. NEXT_PUBLIC_DRIZZLE_DATABASE_URL!);
 export const db = drizzle(sql);
