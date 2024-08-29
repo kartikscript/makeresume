efa16baac4512926c34fa25b946bc372ca76c8fb
Kartikscript <kartik.13102003@gmail.com>
2024-08-29 12:48:40 +0530

    fixing db

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const SigninPage = () => {
-  return (<SignIn/>)
+  return (<SignIn afterSignOutUrl='/'/>)
 }
 
 export default SigninPage
--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -55,13 +55,13 @@ export async function POST(req: Request) {
   const { id } = evt.data
   const eventType = evt.type
   if(eventType==='user.created'){
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name } = evt.data;
 
     const user = {
-      clerkId: 'id',
-      email: 'email_addresses[0].email_address',
-      name: '${first_name} ${last_name}',
-      profilePhoto: 'image_url',
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      profilePhoto: image_url,
     };
 
     const newUser = await db.insert(users).values(user).returning({clerkId:users.clerkId});
