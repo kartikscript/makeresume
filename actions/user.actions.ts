523c99a4b7104c2a0c300778afa28b7b86e48ded
Kartikscript <kartik.13102003@gmail.com>
2024-09-05 21:12:30 +0530

    bug fixing 8

---
+++ actions/user.actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { connect } from "@/lib/db";
+import User from "@/lib/models";
+
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
