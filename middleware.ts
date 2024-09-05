2dac9bd4509ee49c262b8606825b77f3ead14f55
Kartikscript <kartik.13102003@gmail.com>
2024-09-05 20:40:16 +0530

    bug fixing 5

--- middleware.ts
+++ middleware.ts
@@ -28,11 +28,12 @@
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher([ '/user(.*)','/dashboard','resume/(.*)'])
+// const isProtectedRoute = createRouteMatcher([ '/user(.*)','/dashboard','resume/(.*)*'])
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect()
+// })
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
